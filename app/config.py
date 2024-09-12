26371f9a00846ce0398c1f2259f83379b360a052
Jose Gonzalez <gon20335@uvg.edu.gt>
2024-09-12 17:39:28 -0600

    config

--- app/config.py
+++ app/config.py
@@ -3,3 +3,12 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql://root:lacueva2024@localhost/se_chapinas')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Configuración de Flask-Mail
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME', 'tu_correo@gmail.com')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', 'tu_contraseña')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER', 'tu_correo@gmail.com')
